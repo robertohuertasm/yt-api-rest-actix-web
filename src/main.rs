18f7e8ba1a8cb0da3fe059a003b1f5ec7f4882e6
Roberto Huertas <roberto.huertas@outlook.com>
2021-01-24 19:34:26 +0100

    feat(video): fourth video example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod repository;
 mod user;
 
+use actix_web::{web, App, HttpResponse, HttpServer};
+use repository::{MemoryRepository, RepositoryInjector};
 use std::sync::{
     atomic::{AtomicU16, Ordering},
     Arc,
 };
-
-use actix_web::{web, App, HttpResponse, HttpServer};
-use repository::{MemoryRepository, Repository};
 use uuid::Uuid;
 
 #[actix_web::main]
@@ -17,15 +16,15 @@ async fn main() -> std::io::Result<()> {
     // building address
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
     let address = format!("127.0.0.1:{}", port);
-
+    // building shared state
     println!("Starting our server");
     let thread_counter = Arc::new(AtomicU16::new(1));
-    let repo = Arc::new(MemoryRepository::default());
-
+    let repo = RepositoryInjector::new_shared(MemoryRepository::default());
+    // starting the server
     HttpServer::new(move || {
         let thread_index = thread_counter.fetch_add(1, Ordering::SeqCst);
         println!("Starting thread {}", thread_index);
-
+        // starting the services
         App::new()
             .data(thread_index)
             .data(repo.clone())
@@ -53,7 +52,7 @@ async fn main() -> std::io::Result<()> {
 
 async fn get_user(
     user_id: web::Path<String>,
-    repo: web::Data<Arc<MemoryRepository>>,
+    repo: web::Data<Arc<RepositoryInjector>>,
 ) -> HttpResponse {
     if let Ok(parsed_user_id) = Uuid::parse_str(&user_id) {
         match repo.get_user(&parsed_user_id) {
--- src/repository.rs
+++ src/repository.rs
@@ -1,7 +1,28 @@
 use crate::user::User;
+use std::{ops::Deref, sync::Arc};
 
-pub trait Repository {
+pub trait Repository: Send + Sync + 'static {
     fn get_user(&self, user_id: &uuid::Uuid) -> Result<User, String>;
+}
+
+pub struct RepositoryInjector(Box<dyn Repository>);
+
+impl RepositoryInjector {
+    pub fn new(repo: impl Repository) -> Self {
+        Self(Box::new(repo))
+    }
+
+    pub fn new_shared(repo: impl Repository) -> Arc<Self> {
+        Arc::new(Self::new(repo))
+    }
+}
+
+impl Deref for RepositoryInjector {
+    type Target = dyn Repository;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_ref()
+    }
 }
 
 pub struct MemoryRepository {
